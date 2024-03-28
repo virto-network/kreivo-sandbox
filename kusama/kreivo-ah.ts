9a51407d59ffd25aab2aacf0a085fca18dac9e11
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-03-27 22:08:13 -0500

    change(kusama:kreivo-ah): prepare storage

--- kusama/kreivo-ah.ts
+++ kusama/kreivo-ah.ts
@@ -1,12 +1,14 @@
 import {
   BuildBlockMode,
-  getParaId,
+  setStorage,
   setupWithServer,
+  StorageValues,
 } from "@acala-network/chopsticks";
 import {
   connectParachains,
   connectVertical,
 } from "@acala-network/chopsticks-core";
+import { WsProvider } from "@polkadot/rpc-provider";
 
 console.log("Connect Kusama");
 const kusama = await setupWithServer({
@@ -26,11 +28,125 @@ const assetHub = await setupWithServer({
 
 console.log("Connect Kreivo");
 const kreivo = await setupWithServer({
-  endpoint: "wss://kreivo.io/",
+  endpoint: "ws://localhost:20000/",
   "build-block-mode": BuildBlockMode.Instant,
   "runtime-log-level": 5,
   port: 8002,
 });
+
+setStorage(kusama.chain, {
+  System: {
+    Account: [
+      [
+        ["HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F"],
+        {
+          data: {
+            free: 1000000000000000,
+          },
+          providers: 1,
+        },
+      ],
+    ],
+  },
+} as StorageValues);
+
+setStorage(assetHub.chain, {
+  Assets: {
+    Account: [
+      [
+        [1984, "HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F"],
+        {
+          balance: 1000000000000000,
+          status: "Liquid",
+          reason: "Sufficient",
+        },
+      ],
+    ],
+  },
+  System: {
+    Account: [
+      [
+        ["HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F"],
+        {
+          data: {
+            free: 1000000000000000,
+          },
+          providers: 1,
+        },
+      ],
+    ],
+  },
+} as StorageValues);
+
+setStorage(kreivo.chain, {
+  AssetRegistry: {
+    AssetIdMultiLocation: [
+      [
+        [1984],
+        {
+          parents: 1,
+          interior: {
+            X3: [
+              {
+                Parachain: 1000,
+              },
+              {
+                PalletInstance: 50,
+              },
+              {
+                GeneralIndex: 1984,
+              },
+            ],
+          },
+        },
+      ],
+    ],
+    AssetMultiLocationId: [
+      [
+        [
+          {
+            parents: 1,
+            interior: {
+              X3: [
+                {
+                  Parachain: 1000,
+                },
+                {
+                  PalletInstance: 50,
+                },
+                {
+                  GeneralIndex: 1984,
+                },
+              ],
+            },
+          },
+        ],
+        1984,
+      ],
+    ],
+  },
+  Assets: {
+    Asset: [
+      [
+        [1984],
+        {
+          owner: "HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F",
+          issuer: "HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F",
+          admin: "HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F",
+          freezer: "HNZata7iMYWmk5RvZRTiAsSDhV8366zq2YGb3tLH5Upf74F",
+          supply: 0,
+          deposit: 0,
+          minBalance: 70000,
+          isSufficient: true,
+          accounts: 0,
+          sufficients: 0,
+          approvals: 0,
+          status: "Live",
+        },
+      ],
+    ],
+  },
+} as StorageValues);
 
 console.log("Connect Kreivo <> AH");
 await connectParachains([assetHub.chain, kreivo.chain]);
