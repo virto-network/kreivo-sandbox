9942773709574f809fea7cf5cc3a491e5c60c1b2
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-05-19 13:15:53 -0500

    bump

--- lib/endpoints.ts
+++ lib/endpoints.ts
@@ -1,16 +1,24 @@
 export enum ChainIds {
-  assetHub,
   relay,
-  kreivo,
+  assetHub = 1000,
+  encointer = 1001,
+  bridgeHub = 1002,
+  coretime = 1003,
+  people = 1004,
+  kreivo = 2281,
 }
 
 export type ChainId = keyof typeof ChainIds;
 
 export class Endpoint {
   static endpoints: Record<ChainId, string> = {
-    assetHub: "wss://sys.ibp.network/statemine",
     relay: "wss://sys.ibp.network/kusama",
-    kreivo: "wss://kreivo.io",
+    assetHub: "wss://sys.ibp.network/statemine",
+    encointer: "wss://sys.ibp.network/encointer-kusama",
+    bridgeHub: "wss://sys.ibp.network/bridgehub-kusama",
+    coretime: "wss://sys.ibp.network/coretime-kusama",
+    people: "wss://kusama-people-rpc.polkadot.io",
+    kreivo: "wss://kreivo.kippu.rocks",
   };
 
   static get(chainId: ChainId) {
